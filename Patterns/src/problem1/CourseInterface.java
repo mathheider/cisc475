31a163004a8ef6d0080dcd2522758e8d0f0e186e
mathheider <57917849+mathheider@users.noreply.github.com>
2025-04-10 13:13:34 -0400

    Update CourseInterface.java

--- Patterns/src/problem1/CourseInterface.java
+++ Patterns/src/problem1/CourseInterface.java
@@ -1,10 +1,4 @@
 public interface CourseInterface {
-    void setCode(String code);
-    public String getCode();
-    public int getNumber();
-    public String getName();
-    public String getInstructor();
-    public String getBuilding();
-    public int getRoom();
-    public int getCapacity();
+     
+    String getCode();
 }
