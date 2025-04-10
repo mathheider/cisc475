5aeb96846980fff7ca4cc473424fae96728eb4f5
mathheider <57917849+mathheider@users.noreply.github.com>
2025-04-10 13:08:08 -0400

    fixed interfaace

--- Patterns/src/problem1/CourseInterface.java
+++ Patterns/src/problem1/CourseInterface.java
@@ -1,25 +1,18 @@
 public interface CourseInterface {
     void setCode(String code);
     public String getCode();
-    public void setNumber(int number);
     public int getNumber();
-    public void setName(String name);
     public String getName();
-    public void setInstructor(String instructor);
     public String getInstructor();
-    public void setBuilding(String building);
     public String getBuilding();
-    public void setRoom(int room);
     public int getRoom();
-    public void setCapacity(int capacity);
     public int getCapacity();
-    public String toString();
-    public String toStringLong();
     private String code; // e.g., "CISC
 	private int number; // e.g., 475
 	private String name; // e.g. "Software Engineering"
 	private String instructor;
 	private String building;
 	private int room; // room number
+
 	private int capacity; // 
 }
