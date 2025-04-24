2f2a3f5a94ae217bde67658fcc691c3eca41bdc1
mathheider <57917849+mathheider@users.noreply.github.com>
2025-04-24 13:54:58 -0400

    Reapply "Merge branch 'main' of https://github.com/mathheider/cisc475"
    This reverts commit 62067ba04cd9115f78b2b994dbea6dd14308c0b6.

---
+++ NumList/src/edu/udel/cisc475/numlist/EmptyNumList.java
@@ -0,0 +1,44 @@
+package edu.udel.cisc475.numlist;
+
+/** An empty, immutable list of int */
+public class EmptyNumList extends ImmutableNumList {
+
+	public EmptyNumList() {
+	}
+
+	public boolean isEmpty() {
+		return true;
+	}
+
+	public String toString() {
+		return "{}";
+	}
+
+	public NonemptyNumList cons(int val) {
+		return new NonemptyNumList(val, this);
+	}
+
+	public int size() {
+		return 0;
+	}
+
+	public boolean contains(int x) {
+		return false;
+	}
+
+	public int find(int x) {
+		return -1;
+	}
+
+	public NonemptyNumList set(int idx, int val) {
+		throw new IllegalArgumentException("idx exceeds length of list");
+	}
+
+	public boolean equals(Object obj) {
+		return obj instanceof EmptyNumList;
+	}
+
+	public int hashCode() {
+		return 234823; // just some randomly chosen int
+	}
+}
